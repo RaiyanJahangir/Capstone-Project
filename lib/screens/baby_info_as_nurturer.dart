86a4b1d2af2aa6716cfa4f2539b4b098c392c15e
mappyradz449 <72313024+mappyradz449@users.noreply.github.com>
2022-05-24 17:36:25 +0600

    place req list

--- lib/screens/baby_info_as_nurturer.dart
+++ lib/screens/baby_info_as_nurturer.dart
@@ -2,6 +2,7 @@ import 'package:email_password_login/model/babies_model.dart';
 import 'package:email_password_login/screens/FeedingList.dart';
 import 'package:email_password_login/screens/Vaccine_Feeding.dart';
 import 'package:email_password_login/screens/notification_screen.dart';
+import 'package:email_password_login/screens/request.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -22,7 +23,7 @@ String dropdownValue = 'One';
 class nurturer_homepage extends StatefulWidget {
   final String text;
 
-  const nurturer_homepage(@required this.text,{Key? key}) : super(key: key);
+  const nurturer_homepage(@required this.text, {Key? key}) : super(key: key);
 
   @override
   State<nurturer_homepage> createState() => _nurturer_homepageState();
@@ -172,8 +173,7 @@ class _nurturer_homepageState extends State<nurturer_homepage> {
                             ])),
                   ),
                   Expanded(
-                    child: Text(
-                        "${loggedInbaby.name}",
+                    child: Text("${loggedInbaby.name}",
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           fontSize: 20,
@@ -257,7 +257,8 @@ class _nurturer_homepageState extends State<nurturer_homepage> {
                   Card(
                     elevation: 4,
                     child: InkWell(
-                      onTap: () => null,
+                      onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (c) => AddRequest())),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
---
+++ lib/screens/request.dart
@@ -0,0 +1,226 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_password_login/screens/notification_screen.dart';
+import 'package:email_password_login/screens/profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../model/user_model.dart';
+
+class AddRequest extends StatefulWidget {
+  const AddRequest({Key? key}) : super(key: key);
+
+  @override
+  State<AddRequest> createState() => _AddRequestState();
+}
+
+class _AddRequestState extends State<AddRequest> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+  List? Access;
+  List? nAccess;
+  int itemCount = 0;
+  int nitemCount = 0;
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("Users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      // ignore: unnecessary_this
+      this.loggedInUser = UserModel.fromMap(value.data());
+      setState(() {
+        Access = loggedInUser.gaccess;
+        nAccess = loggedInUser.naccess;
+        if (Access!.isNotEmpty) {
+          itemCount = Access!.length;
+        }
+        if (nAccess!.isNotEmpty) {
+          nitemCount = nAccess!.length;
+        }
+        print(loggedInUser.name);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final db = FirebaseFirestore.instance;
+    return Scaffold(
+      appBar: AppBar(
+        // leading: IconButton(
+        //   icon: Icon(Icons.person),
+        //   onPressed: () {},
+        // ),
+        centerTitle: true,
+        title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(child: Text('Place Request')),
+              IconButton(
+                icon: Icon(
+                  Icons.circle_notifications,
+                  color: Colors.white,
+                  size: 20,
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (c) => NotificationScreen()));
+                },
+              ),
+            ]),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+              PopupMenuItem(
+                child: ListTile(
+                  //var a;
+                  leading: Icon(
+                    Icons.account_circle,
+                    color: Colors.blue,
+                    size: 24.0,
+                  ),
+                  //title: const Text(size ?? ''),
+                  title: Text(
+                    "User Profile",
+                  ),
+                  subtitle: Text(
+                    "${loggedInUser.name}",
+                  ),
+                  //onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (c) => SensorScreen())),
+                  onTap: () => Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (c) => Home())),
+                ),
+              ),
+              PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.logout,
+                    color: Colors.blue,
+                  ),
+                  title: Text('Logout'),
+                  onTap: () => showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Confirm Logging Out ?",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                  color: Colors.blue,
+                                  fontSize: 25)),
+                          actions: <Widget>[
+                            TextButton(
+                                onPressed: () {
+                                  logout(context);
+                                },
+                                child: Text("YES",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontStyle: FontStyle.italic,
+                                        color: Colors.blueAccent,
+                                        fontSize: 20))),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("NO",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontStyle: FontStyle.italic,
+                                        color: Colors.blueAccent,
+                                        fontSize: 20)))
+                          ],
+                        );
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ],
+        //backgroundColor: Color.fromRGBO(232, 232, 242, 1),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: db.collection('Babies').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else
+            return ListView(
+              children: snapshot.data!.docs.map((doc) {
+                return Card(
+                  child: ListTile(
+                    title: Text(doc.id),
+                  ),
+                );
+              }).toList(),
+            );
+        },
+        // ignore: prefer_const_constructors
+        // body: SingleChildScrollView(
+        //   child: Row(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     crossAxisAlignment: CrossAxisAlignment.center,
+        //     children: <Widget>[
+        //       // ignore: prefer_const_constructors
+        //       Text('List of Babies: ',
+        //           textAlign: TextAlign.center,
+        //           // ignore: prefer_const_constructors
+        //           style: TextStyle(
+        //               fontSize: 22,
+        //               color: Colors.blue,
+        //               fontWeight: FontWeight.bold,
+        //               //fontStyle: FontStyle.italic,
+        //               // ignore: prefer_const_literals_to_create_immutables
+        //               shadows: [
+        //                 // ignore: prefer_const_constructors
+        //                 Shadow(
+        //                     color: Colors.blueAccent,
+        //                     // ignore: prefer_const_constructors
+        //                     offset: Offset(2, 1),
+        //                     blurRadius: 10)
+        //               ])),
+        //       Expanded(
+        //           child: StreamBuilder<QuerySnapshot>(
+        //         stream: db.collection('Users').snapshots(),
+        //         builder: (context, snapshot) {
+        //           if (!snapshot.hasData) {
+        //             // ignore: prefer_const_constructors
+        //             return Center(
+        //               // ignore: prefer_const_constructors
+        //               child: CircularProgressIndicator(),
+        //             );
+        //           } else
+        //             return ListView(
+        //               children: snapshot.data!.docs.map((doc) {
+        //                 return Card(
+        //                   child: ListTile(
+        //                     title: Text(doc.id),
+        //                   ),
+        //                 );
+        //               }).toList(),
+        //             );
+        //         },
+        //       )),
+        //     ],
+        //   ),
+        // ),
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).popUntil((route) => route.isFirst);
+  }
+}
